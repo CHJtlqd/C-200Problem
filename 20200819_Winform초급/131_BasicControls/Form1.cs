f9eaa46ca717e51902efa46d48868dd58adb2f48
CHJtlqd <tlqd123@outlook.com>
2020-08-19 18:27:25 +0900

    20200819

---
+++ 20200819_Winform초급/131_BasicControls/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _131_BasicControls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 텍스트박스에 이름을 입력하지 않았을 때
+            if (textBox1.Text == "")
+            {
+                label2.Text = "";
+                MessageBox.Show("이름을 입력하세요", "경고");
+            }
+            else
+            {
+                label2.Text = textBox1.Text+"님! 안녕하세요!";
+            }
+        }
+    }
+}
---
+++ 20200819_Winform초급/135_RadioButton/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _135_RadioButton
+{
+    public partial class Form1 : Form
+    {
+        private RadioButton checkedRB;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result = "국적: ";
+            foreach (RadioButton item in groupBox1.Controls)
+            {
+                if (item.Checked)
+                {
+                    result += item.Text + "\n";
+                    break;
+                }
+            }
+            foreach (RadioButton item in groupBox2.Controls)
+            {
+                if (item.Checked)
+                {
+                    result += "성별: " + item.Text;
+                    break;
+                }
+            }
+            //if (rbKorea.Checked)
+            //    result += "국적: 대한민국\n";
+            //else if(rbChina.Checked)
+            //    result += "국적: 중국\n";
+            //else if (rbJapan.Checked)
+            //    result += "국적: 일본\n";
+            //else if (rbOthers.Checked)
+            //    result += "국적: 그 외 국가\n";
+
+            //if (checkedRB == rbMale)
+            //    result += "성별: 남성";
+            //else if (checkedRB == rbFemale)
+            //    result += "성별: 여성";
+
+            MessageBox.Show(result, "Result");
+        }
+
+        private void rbMale_CheckedChanged(object sender, EventArgs e)
+        {
+            checkedRB = rbMale;
+        }
+
+        private void rbFemale_CheckedChanged(object sender, EventArgs e)
+        {
+            checkedRB = rbFemale;
+
+        }
+    }
+}
